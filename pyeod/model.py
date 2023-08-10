adad5ecb1454dc2e1a2bce89e30777e48fb8c09a
Ray Chen <tankimarshal2@gmail.com>
2023-08-10 21:27:23 +0100

    Fix pylance issues

--- pyeod/model.py
+++ pyeod/model.py
@@ -53,7 +53,7 @@ class Poll:
 
 class ElementPoll(Poll):
     def __init__(
-        self, author: User, combo: Tuple[Element], result: str, exists: bool
+        self, author: User, combo: Tuple[Element, ...], result: str, exists: bool
     ) -> None:
         super(ElementPoll, self).__init__(author)
         self.combo = combo
@@ -62,7 +62,7 @@ class ElementPoll(Poll):
 
     def resolve(self, database):
         element = Element(
-            self.result, self.author, time.time(), len(database.elements) + 1
+            self.result, self.author, round(time.time()), len(database.elements) + 1
         )
         database.elements[self.result.lower()] = element
         database.set_combo_result(self.combo, element)
@@ -72,8 +72,8 @@ class Database:
     def __init__(
         self,
         elements: Dict[str, Element],
-        starters: List[Element],
-        combos: Dict[Tuple[Element], Element],
+        starters: Tuple[Element, ...],
+        combos: Dict[Tuple[Element, ...], Element],
         users: Dict[int, User],
         polls: List[Poll],
     ) -> None:
@@ -84,7 +84,7 @@ class Database:
         self.polls = polls
 
     @staticmethod
-    def new_db(starter_elements: List[Element]) -> "Database":
+    def new_db(starter_elements: Tuple[Element, ...]) -> "Database":
         return Database(
             elements={i.name.lower(): i for i in starter_elements},
             starters=starter_elements,
@@ -96,13 +96,13 @@ class Database:
     def has_element(self, element: str) -> bool:
         return element.lower() in self.elements
 
-    def get_combo_result(self, combo: Tuple[Element]) -> Union[Element, None]:
+    def get_combo_result(self, combo: Tuple[Element, ...]) -> Union[Element, None]:
         sorted_combo = tuple(sorted(combo))
         if sorted_combo in self.combos:
             return self.combos[sorted_combo]
         return None
 
-    def set_combo_result(self, combo: Tuple[Element], result: Element) -> None:
+    def set_combo_result(self, combo: Tuple[Element, ...], result: Element) -> None:
         sorted_combo = tuple(sorted(combo))
         if sorted_combo in self.combos:
             raise InternalError("Combo exists", "The combo already exists")
@@ -119,14 +119,15 @@ DEFAULT_STARTER_ELEMENTS = (AIR, EARTH, FIRE, WATER)
 class GameInstance:
     def __init__(
         self,
-        starter_elements: Optional[List[Element]] = None,
+        starter_elements: Optional[Tuple[Element, ...]] = None,
         db: Optional[Database] = None,
         vote_req: int = 4,
         poll_limit: int = 21,
     ) -> None:
-        self.starter_elements = starter_elements
-        if self.starter_elements is None:
+        if starter_elements is None:
             self.starter_elements = copy.deepcopy(DEFAULT_STARTER_ELEMENTS)
+        else:
+            self.starter_elements = starter_elements
         if db == None:
             self.db = Database.new_db(self.starter_elements)
         else:
@@ -157,8 +158,8 @@ class GameInstance:
             )
         return element
 
-    def combine(self, user: User, combo: Tuple[str]) -> Element:
-        element_combo = (self.check_element(name, user) for name in combo)
+    def combine(self, user: User, combo: Tuple[str, ...]) -> Element:
+        element_combo = tuple(self.check_element(name, user) for name in combo)
         result = self.db.get_combo_result(element_combo)
         if result is None:
             raise GameError("Not a combo", "The combo requested does not exist")
@@ -169,11 +170,10 @@ class GameInstance:
         user.inv.append(result)
         return result
 
-    def suggest_element(self, user: User, combo: Tuple[str], result: str) -> Poll:
+    def suggest_element(self, user: User, combo: Tuple[str, ...], result: str) -> Poll:
         if user.active_polls > self.poll_limit:
             raise GameError("Too many active polls")
-        # Technically not needed since combine already checks
-        element_combo = (self.check_element(name, user) for name in combo)
+        element_combo = tuple(self.check_element(name, user) for name in combo)
         poll = ElementPoll(user, element_combo, result, self.db.has_element(result))
         self.db.polls.append(poll)
         user.active_polls += 1
