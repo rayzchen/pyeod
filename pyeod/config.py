b3e9d532d7e87b4b035c154026a19906cb2b66a3
Cheesy-Brik <loweryan168@yahoo.com>
2023-08-09 21:42:43 -0500

    Fix Spelling Mistake

--- pyeod/config.py
+++ pyeod/config.py
@@ -3,7 +3,7 @@ import os
 package = os.path.dirname(os.path.abspath(__file__))
 control = os.path.join(package, "control")
 stopfile = os.path.join(control, "stop")
-restartile = os.path.join(control, "restart")
+restartfile = os.path.join(control, "restart")
 
 # Debug server used to test new commands
 main_server = 0
