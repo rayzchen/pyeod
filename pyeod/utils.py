39aabc6e208bc18ec1d592d920041213d3079df7
Ray Chen <tankimarshal2@gmail.com>
2023-08-11 18:03:48 +0100

    Only display cause of exception

--- pyeod/utils.py
+++ pyeod/utils.py
@@ -2,6 +2,6 @@ import traceback
 
 
 def format_traceback(err):
-    _traceback = "".join(traceback.format_tb(err.__traceback__))
+    _traceback = "".join(traceback.format_exception(type(err), err, err.__traceback__))
     error = f"```py\n{_traceback}{type(err).__name__}: {err}\n```"
     return error
