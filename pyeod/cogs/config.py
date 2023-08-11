e414f4d9fd2ae6a80c1412b14840a8e8baa4a2bb
Cheesy-Brik <loweryan168@yahoo.com>
2023-08-10 19:31:24 -0500

    Move manager to bot, and added more config commands

--- pyeod/cogs/config.py
+++ pyeod/cogs/config.py
@@ -12,45 +12,75 @@ import os
 class Config(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.manager = None
+        self.bot.manager = None  # Move to dif module???
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Loading instance manager")
-        self.manager = InstanceManager()
+        self.bot.manager = InstanceManager()
         # TODO: load manager here?
 
     @commands.Cog.listener("on_message")
     async def check_for_new_servers(self, msg: Message):
-        if not self.manager:  # Messages can be caught before bot is ready
+        if not self.bot.manager:  # Messages can be caught before bot is ready
             return
         if msg.guild and msg.guild not in self.manager:
-            self.manager.add_instance(msg.guild.id, DiscordGameInstance())
+            self.bot.manager.add_instance(msg.guild.id, DiscordGameInstance())
 
     # Slash commands only cus converting to channel is busted
     @commands.slash_command()
     async def add_play_channel(self, ctx: bridge.BridgeContext, channel: TextChannel):
         # TODO: Add permissions locks so that only certain roles can add channels
 
-        if ctx.guild and ctx.guild.id not in self.manager:
-            self.manager.add_instance(ctx.guild.id, DiscordGameInstance())
+        if ctx.guild and ctx.guild.id not in self.bot.manager:
+            self.bot.manager.add_instance(ctx.guild.id, DiscordGameInstance())
 
-        # If converting is fixed
-        # if isinstance(
-        #    ctx, bridge.BridgeExtContext
-        # ):  # If it's a text command convert the channel arg to an actual channel object
-        #    if "<#" in channel:
-        #        channel = self.bot.get_channel(
-        #            int(channel.replace("<#", "").replace(">", ""))
-        #        )
-        #    else:
-        #        channel = get(ctx.guild.channels, name=channel)
-
-        server = self.manager.get_instance(ctx.guild.id)
+        server = self.bot.manager.get_instance(ctx.guild.id)
 
         server.channels.play_channels.append(channel)
 
         await ctx.respond(f"Successfully added {channel.name} as a play channel!")
+
+    @commands.slash_command()
+    async def remove_play_channel(self, ctx: bridge.BridgeContext, channel: TextChannel):
+        # TODO: Add permissions locks so that only certain roles can add channels
+
+        if ctx.guild and ctx.guild.id not in self.bot.manager:
+            self.bot.manager.add_instance(ctx.guild.id, DiscordGameInstance())
+
+        server = self.bot.manager.get_instance(ctx.guild.id)
+
+        try:
+            server.channels.play_channels.remove(channel)
+            await ctx.respond(f"Successfully removed {channel.name} as a play channel!")
+        except ValueError:
+            await ctx.respond(f"That is not a play channel")
+
+    @commands.slash_command()
+    async def add_news_channel(self, ctx: bridge.BridgeContext, channel: TextChannel):
+        # TODO: Add permissions locks so that only certain roles can add channels
+
+        if ctx.guild and ctx.guild.id not in self.bot.manager:
+            self.bot.manager.add_instance(ctx.guild.id, DiscordGameInstance())
+
+        server = self.bot.manager.get_instance(ctx.guild.id)
+
+        server.channels.news_channel = channel
+
+        await ctx.respond(f"Successfully added {channel.name} as the news channel!")
+
+    @commands.slash_command()
+    async def add_voting_channel(self, ctx: bridge.BridgeContext, channel: TextChannel):
+        # TODO: Add permissions locks so that only certain roles can add channels
+
+        if ctx.guild and ctx.guild.id not in self.bot.manager:
+            self.bot.manager.add_instance(ctx.guild.id, DiscordGameInstance())
+
+        server = self.bot.manager.get_instance(ctx.guild.id)
+
+        server.channels.voting_channel = channel
+
+        await ctx.respond(f"Successfully added {channel.name} as the voting channel!")
 
     @bridge.bridge_command()
     async def user(self, ctx: bridge.BridgeContext, user: User):
