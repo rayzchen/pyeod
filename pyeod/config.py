c65ac19d85073775ab92a133e83b3c7581d1c6d7
Ray Chen <tankimarshal2@gmail.com>
2023-08-12 09:57:54 +0100

    Hide user directories in error logs

--- pyeod/config.py
+++ pyeod/config.py
@@ -1,6 +1,7 @@
 import os
 
 package = os.path.dirname(os.path.abspath(__file__))
+package_location = os.path.dirname(package)
 control = os.path.join(package, "control")
 stopfile = os.path.join(control, "stop")
 restartfile = os.path.join(control, "restart")
--- pyeod/utils.py
+++ pyeod/utils.py
@@ -1,7 +1,18 @@
 import traceback
+from pyeod import config
+import os
+import sysconfig
 
 
 def format_traceback(err):
-    _traceback = "".join(traceback.format_exception(type(err), err, err.__traceback__))
-    error = f"```py\n{_traceback}{type(err).__name__}: {err}\n```"
+    _traceback = "".join(traceback.format_exception(
+        type(err),
+        err,
+        err.__traceback__
+    ))
+    error = f"```py\n{_traceback}\n```"
+    workdir = os.path.abspath("/<workdir>")
+    packagedir = os.path.abspath("/<packages>")
+    error = error.replace(config.package_location, workdir)
+    error = error.replace(sysconfig.get_paths()["stdlib"], packagedir)
     return error
