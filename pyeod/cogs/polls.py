dbb0220e933d32aa53b392a0956d892ea2f85991
Ray Chen <tankimarshal2@gmail.com>
2023-08-13 08:59:16 +0100

    Reformat poll code

--- pyeod/cogs/polls.py
+++ pyeod/cogs/polls.py
@@ -1,17 +1,14 @@
 from discord.ext import commands, tasks, bridge
 from discord.utils import get
-from discord import Message, TextChannel, default_permissions
-from pyeod.frontend import DiscordGameInstance, InstanceManager
-from pyeod.packer import save_instance, load_instance
-from pyeod import config
-import glob
-import os
+from discord import Message
+from pyeod.frontend import InstanceManager, Poll
+from typing import Dict
 
 class Polls(commands.Cog):
     def __init__(self, bot):
         self.bot:bridge.AutoShardedBot = bot
         self.check_polls.start()
-    
+
     @tasks.loop(seconds=1)
     async def check_polls(self):
         for guild_id, server in InstanceManager.current.instances.items():
@@ -21,8 +18,9 @@ class Polls(commands.Cog):
                 continue
             voting_channel = await self.bot.fetch_channel(server.channels.voting_channel)
             messages = await voting_channel.history(limit=50, oldest_first=True).flatten()
-            messages = [message for message in messages if message.author.id == self.bot.user.id]#Filter out non bot messages
-            poll_dict = {}
+            # Only select bot messages
+            messages = [message for message in messages if message.author.id == self.bot.user.id]
+            poll_messages: Dict[Poll, Message] = {}
             for message, poll in zip(messages, server.db.polls):
                 poll.votes = 0
                 try:
@@ -30,21 +28,25 @@ class Polls(commands.Cog):
                     upvote_count = get(message.reactions, emoji = '\u2B06\uFE0F').count
                     downvote_count = downvotes.count
                 except:
-                    continue#Just break out of the loop if the above code breaks
+                    # TODO: handle exceptions properly
+                    # Just break out of the loop if the above code breaks
+                    continue
                 if get(await downvotes.users().flatten(), id = poll.author.id):
-                    poll.votes -= server.vote_req * 2 #Double it and give it to the next person
+                    # Double it and give it to the next person
+                    poll.votes -= server.vote_req * 2
                 else:
-                    poll.votes += upvote_count - 1# Do not include the bot's own reaction
+                    # Do not include the bot's own reaction
+                    poll.votes += upvote_count - 1
                     poll.votes -= downvote_count - 1
-                poll_dict[poll] = message
-            news_polls = []
-            for i in server.check_polls():#Delete messages before we send to news
-                await poll_dict[i].delete()
-                news_polls.append(i)
+                poll_messages[poll] = message
+            accepted_polls = server.check_polls()
+            # Delete messages before we send to news
+            for poll in accepted_polls:
+                await poll_messages[poll].delete()
             if server.channels.news_channel:
                 news_channel = await self.bot.fetch_channel(server.channels.news_channel)
-                for i in news_polls:
-                    await news_channel.send(server.convert_poll_to_news_message(i))
-                
+                for poll in accepted_polls:
+                    await news_channel.send(server.convert_poll_to_news_message(poll))
+
 def setup(client):
     client.add_cog(Polls(client))
