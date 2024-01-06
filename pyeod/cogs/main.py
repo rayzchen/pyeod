813d89cbb57a96c979ff844fd7fd66626d45e102
Cheesy-Brik <loweryan168@yahoo.com>
2024-01-06 02:49:02 -0600

    Fix !help

--- pyeod/cogs/main.py
+++ pyeod/cogs/main.py
@@ -123,7 +123,7 @@ class Main(commands.Cog):
             try:
                 if not await command.can_run(ctx):
                     continue
-            except CheckFailure:
+            except:
                 continue
 
             embed = Embed(
